0551e5bbae0d0581800f87827483fb8374225b6d
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-01-17 17:07:27 -0700

    initial project fresh off the cmake template

---
+++ Omnoptic/Omnoptic.cpp
@@ -0,0 +1,16 @@
+﻿// Omnoptic.cpp : Defines the entry point for the application.
+//
+
+#include "Omnoptic.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Initializing Omnoptic..." << endl;
+
+	// Omnoptic initialization
+
+	cout << "Omnoptic online." << endl;
+	return 0;
+}
